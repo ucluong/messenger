3f3707d30be1a275f0328fb81453415c854ad1bd
Kevin <ninhngoc.t4.bkdn@gmail.com>
2022-12-29 21:01:22 +0700

    done login

--- app/src/main/java/com/example/fastchatmesss/ui/login/LoginActivity.kt
+++ app/src/main/java/com/example/fastchatmesss/ui/login/LoginActivity.kt
@@ -1,23 +1,61 @@
 package com.example.fastchatmesss.ui.login
 
 
+import android.content.Intent
+import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.example.fastchatmesss.R
 import com.example.fastchatmesss.base.BaseActivity
+import com.example.fastchatmesss.ui.message.MessengerActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_login.*
 
+
 class LoginActivity : BaseActivity(R.layout.activity_login) {
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        auth = Firebase.auth
+
+    }
+
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if(currentUser != null){
+            val intent= Intent(this, MessengerActivity::class.java)
+            intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+
 
     override fun initActions() {
         btnLogin.setOnClickListener {
             val email= edtLoginEmail.text.toString()
-            val pass= edtLoginPasss.text.toString()
+            val password= edtLoginPasss.text.toString()
             Log.d("Login", "Attempt login with email/pw: $email/****" )
+
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val intent= Intent(this, MessengerActivity::class.java)
+                        intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                        startActivity(intent)
+                    } else {
+                        Toast.makeText(baseContext, "Authentication failed.", Toast.LENGTH_SHORT).show()
+                    }
+                }
+
         }
         tvBackTo.setOnClickListener {
             finish()
         }
-
     }
 
 
@@ -28,8 +66,5 @@ class LoginActivity : BaseActivity(R.layout.activity_login) {
     override fun initViews() {
 
     }
-
-
-
 
 }
--- app/src/main/java/com/example/fastchatmesss/ui/signup/RegisterActivity.kt
+++ app/src/main/java/com/example/fastchatmesss/ui/signup/RegisterActivity.kt
@@ -7,6 +7,7 @@ import android.net.Uri
 import android.provider.MediaStore
 import android.util.Log
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import com.example.fastchatmesss.ui.message.MessengerActivity
 import com.example.fastchatmesss.R
 import com.example.fastchatmesss.base.BaseActivity
@@ -15,39 +16,25 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_regisrer.*
 
 class RegisterActivity : BaseActivity(R.layout.activity_regisrer) {
-
-
+    var selectPhotoUri : Uri? = null
 
     override fun initActions() {
         // code btnDangki
         btnDangki.setOnClickListener {
             val email = edtEmail.text.toString()
             val password = edtPass.text.toString()
-
-
             if (email.isEmpty() || password.isEmpty()) {
                 Toast.makeText(this, "vui lòng nhập nội dung trong email/pass ", Toast.LENGTH_SHORT)
                     .show()
                 return@setOnClickListener
             }
-            Log.d("RegisterActivity", " Emmailis:" + email)
-            Log.d("RegisterActivity", "Password:$password")
-
             // Xác thực Firebase để tạo người dùng bằng email mật khẩu
             FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener {
                     if (!it.isSuccessful) return@addOnCompleteListener
-                    // eles if succerrful
-                    Log.d(
-                        "RegisterActivity",
-                        "đã tạo thành công ng dùng với uid:${it.result.user?.uid}")
-
                     val intent= Intent(this, MessengerActivity::class.java)
                     intent.flags= Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
                     startActivity(intent)
-
-
-
                 }
                 .addOnFailureListener {
                     Toast.makeText(this, "ko thanhf coong :${it.message}", Toast.LENGTH_SHORT)
@@ -60,44 +47,28 @@ class RegisterActivity : BaseActivity(R.layout.activity_regisrer) {
     override fun initViews() {
         // code phần tvAlready
         tvAlready.setOnClickListener {
-            Log.d("MainActivity", "Bạn đã có tài khoản")
-
-            //
             val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
-
         }
 
     }
+
+
     override fun initToolbar() {
         // code btn chọn ảnh avata
         btnPhoto.setOnClickListener {
-            Log.d("RegisterActivity", " Try to whow photo selector")
-            val intent = Intent(Intent.ACTION_PICK)
-            intent.type = "image/*"
-            startActivityForResult(intent, 0)
-
+            getContentImage.launch("image/*")
         }
     }
 
-    var photoUri: Uri? = null
-
-    //  // code btn chọn ảnh avata
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == 0 && resultCode == Activity.RESULT_OK && data != null) {
-            // tiếp tục và kiểm tra xem hình ảnh đã chọn là gì
-            Log.d(" RegisterActivity", " photo was selected")
-            photoUri = data.data
-            val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
-            val bitmapDrawable = BitmapDrawable(bitmap)
-            btnPhoto.setBackgroundDrawable(bitmapDrawable)
-        }
+    private val getContentImage = registerForActivityResult(ActivityResultContracts.GetContent()) { uri: Uri? ->
+       uri?.let {
+           selectPhotoUri = uri
+           val bitmap = MediaStore.Images.Media.getBitmap(contentResolver, uri)
+           btnPhoto.setImageBitmap(bitmap)
+//           val bitmapDrawable = BitmapDrawable(bitmap)
+//           btnPhoto.setBackgroundDrawable(bitmapDrawable)
+       }
     }
-
-    // code cho Username
-
-    class  User(val uid:String, val username:String, val  imageUri:String)
-
 
 }
