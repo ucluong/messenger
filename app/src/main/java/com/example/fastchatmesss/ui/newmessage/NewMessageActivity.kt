2cf54013214692412d014a1db856dc6dc60b6eea
Kevin <ninhngoc.t4.bkdn@gmail.com>
2022-12-30 22:13:19 +0700

    view holder

--- app/src/main/java/com/example/fastchatmesss/ui/newmessage/NewMessageActivity.kt
+++ app/src/main/java/com/example/fastchatmesss/ui/newmessage/NewMessageActivity.kt
@@ -1,14 +1,31 @@
 package com.example.fastchatmesss.ui.newmessage
 
-
-
 import com.example.fastchatmesss.R
 import com.example.fastchatmesss.base.BaseActivity
-
+import kotlinx.android.synthetic.main.activity_new_message.*
 
 class NewMessageActivity : BaseActivity(R.layout.activity_new_message) {
+    private val newMessageAdapter by lazy { NewMessageAdapter() }
+
 
     override fun initViews() {
+        recyViewNewMesss.apply {
+            adapter = newMessageAdapter
+        }
+
+        val dataFake = mutableListOf<String>(
+            "Linhtesst",
+            "Linhtesst",
+            "Linhtesst",
+            "Linhtesst",
+            "Linhtesst",
+            "Linhtesst",
+            "Linhtesst",
+            "Linhtesst",
+            "Linhtesst",
+            "Linhtesst",
+        )
+        newMessageAdapter.data = dataFake
     }
 
     override fun initActions() {
---
+++ app/src/main/java/com/example/fastchatmesss/ui/newmessage/NewMessageAdapter.kt
@@ -0,0 +1,68 @@
+package com.example.fastchatmesss.ui.newmessage
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.fastchatmesss.R
+import com.example.fastchatmesss.data.User
+import kotlinx.android.synthetic.main.item_me.view.*
+import kotlinx.android.synthetic.main.item_you.view.*
+
+const val VIEW_TYPE_ME = 1
+const val VIEW_TYPE_YOU = 2
+
+class NewMessageAdapter (val onClick : (User) -> Unit ): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    var data = mutableListOf<String>()
+        @SuppressLint("NotifyDataSetChanged")
+        set(value) {
+            field = value
+            notifyDataSetChanged()
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return when (viewType) {
+            VIEW_TYPE_ME -> {
+                val view =
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_me, parent, false)
+                ViewHolderMe(view)
+            }
+            else -> {
+                val view =
+                    LayoutInflater.from(parent.context).inflate(R.layout.item_you, parent, false)
+                ViewHolderYou(view)
+            }
+        }
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        if (position  % 2 == 0) {
+            return VIEW_TYPE_ME
+        } else {
+            return VIEW_TYPE_YOU
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when(holder.itemViewType){
+            VIEW_TYPE_ME -> {(holder as? ViewHolderMe)?.bind(data[position])}
+            VIEW_TYPE_YOU -> {(holder as? ViewHolderYou)?.bind(data[position])}
+        }
+    }
+
+    override fun getItemCount(): Int = data.size
+
+
+    inner class ViewHolderMe(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(string: String) {
+            itemView.tvMessageMe.text = string
+        }
+    }
+
+    inner class ViewHolderYou(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(string: String) {
+            itemView.tvMessageYou.text = string
+        }
+    }
+}
